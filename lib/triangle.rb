099ab6339a25cb11396bc2bbe758e5b818316a4b
Gucci Fan <gucc@umich.edu>
2022-12-27 21:28:38 -0500

    commit tree a2f8791206117e9a60d30778ddacfab460f31e7a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+  def initialize(one, two, three)
+    @one=one
+    @two=two
+    @three=three
+  end
+
+  class TriangleError < StandardError
+  end
+
+  def kind
+
+    def invalid?
+      if one<=0 
+        true
+      elsif two<=0 
+        true
+      elsif three<=0
+        true
+      elsif one+two<=three
+        true
+      elsif two+three<=one
+        true
+      elsif one+three<=two
+        true
+      else false
+      end
+    end
+
+    if self.invalid?
+      raise TriangleError
+    elsif one==two && two==three
+      :equilateral
+    elsif one==two || two==three || three==one
+      :isosceles
+    elsif one!=two && two!=three && three!=one
+      :scalene
+    end
+  end
+  #binding.pry
 end
